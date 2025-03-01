3e0a022328067b0afc7de21114e61abfaf0da82b
Endyzi <private_nilhamn@hotmail.com>
2025-03-01 11:41:20 +0100

    added model for character api response

---
+++ Backend/Models/CharacterApiResponse.cs
@@ -0,0 +1,82 @@
+using System.Text.Json.Serialization;
+
+namespace Backend.Models
+{
+  public class CharacterApiResponse
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("realm")]
+    public CharacterRealmInfo Realm { get; set; }
+
+    [JsonPropertyName("faction")]
+    public FactionInfo Faction { get; set; }
+
+    [JsonPropertyName("race")]
+    public RaceInfo Race { get; set; }
+
+    [JsonPropertyName("character_class")]
+    public CharacterClassInfo CharacterClass { get; set; }
+
+    [JsonPropertyName("active_spec")]
+    public SpecInfo ActiveSpec { get; set; }
+
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("average_item_level")]
+    public int AverageItemLevel { get; set; }
+
+    [JsonPropertyName("equipped_item_level")]
+    public int EquippedItemLevel { get; set; }
+
+    [JsonPropertyName("active_title")]
+    public TitleInfo ActiveTitle { get; set; }
+
+    [JsonPropertyName("media")]
+    public MediaInfo Media { get; set; }
+}
+
+public class CharacterRealmInfo
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public class FactionInfo
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public class RaceInfo
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public class CharacterClassInfo
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public class SpecInfo
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public class TitleInfo
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public class MediaInfo
+{
+    [JsonPropertyName("href")]
+    public string Href { get; set; }
+}
+}
