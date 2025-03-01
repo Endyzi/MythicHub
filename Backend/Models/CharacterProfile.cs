ecdf0f2943c0536a37529b879806ce04082ddbb3
Endyzi <private_nilhamn@hotmail.com>
2025-03-01 11:40:38 +0100

    added character model to access json values

---
+++ Backend/Models/CharacterProfile.cs
@@ -0,0 +1,19 @@
+namespace Backend.Models
+{
+
+    //will probably add more values later on, achievements maybe or something else.
+    public class CharacterProfile
+    {
+        public string Name { get; set; }
+        public string Realm { get; set; }
+        public string Faction { get; set; }
+        public string Race { get; set; }
+        public string CharacterClass { get; set; }
+        public string Specialization { get; set; }
+        public int Level { get; set; }
+        public int AverageItemLevel { get; set; }
+        public int EquippedItemLevel { get; set; }
+        public string Title { get; set; }
+        public string CharacterImage { get; set; }
+    }
+}
